45fc8a632c4cd64d79daf621f7c56f7b5aa7b340
+ <+>
2022-09-22 18:57:54 -0400

    commit tree 5fdcabd13b620fc72c8d7cef2568626246b6426f

---
+++ index.js
@@ -0,0 +1,19 @@
+
+
+add = (c, d) => {
+    const result = c + d;
+    return result;
+
+}
+function divide(a, b) {
+    console.log(`"this is a ${a}"`); 
+    console.log(`"this is b ${b}"`); 
+    const sum = a / b 
+    
+    return sum;
+}
+
+ square = (a) => {
+    const result = a * a;
+    return result;
+};
